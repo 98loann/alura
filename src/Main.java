a6e9c60214be08413126a3f2d3f5ec982164f1e9
98loann <98luan@gmail.com>
2023-04-13 21:23:57 -0300

    Atualizando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Concluií a aula 01 e agora estou mergulhando em Java!!!");
     }
 }
