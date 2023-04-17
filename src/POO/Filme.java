fd28f2d3af8860b5e9e4811d6b55284fd9b22cc0
98loann <98luan@gmail.com>
2023-04-17 10:03:07 -0300

    Atualizando

--- src/POO/Filme.java
+++ src/POO/Filme.java
@@ -4,7 +4,23 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " +nome);
+        System.out.println("Ano de lançamento: " +anoDeLancamento);
+        System.out.println("Duração em Minutos: " +duracaoEmMinutos);
+        System.out.println("Avaliação: " +totalDeAvaliacoes);
+        System.out.println("Incluído no plano: " +incluidoNoPlano);
+    }
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+    double pegaMedia(){
+        return somaDasAvaliacoes/totalDeAvaliacoes;
+    }
 }
