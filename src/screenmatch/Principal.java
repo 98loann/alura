d8e69ff0f6dfc12223f0b114506a2bd53fcbc4a5
98loann <98luan@gmail.com>
2023-04-17 11:14:41 -0300

    Atualizando

--- src/screenmatch/Principal.java
+++ src/screenmatch/Principal.java
@@ -1,4 +1,4 @@
-package POO;
+package screenmatch;
 
 public class Principal {
     public static void main(String[] args) {
@@ -11,9 +11,7 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " +meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
-
     }
 }
