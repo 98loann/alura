4794f15245ab22aba631fdb42722ba718ab54e2d
98loann <98luan@gmail.com>
2023-04-20 10:36:44 -0300

    ...

--- src/screenmatch/Principal/principalComListas.java
+++ src/screenmatch/Principal/principalComListas.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 public class principalComListas {
     public static void main(String[] args) {
         Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
         Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
         var filmeDoLuan = new Filme("O Fabuloso Destino de Amélie Poulain", 2002);
+        filmeDoLuan.avalia(9);
         Serie lost = new Serie("Lost", 2000);
 
         ArrayList<Titulo> lista = new ArrayList<>();
@@ -18,6 +21,10 @@ public class principalComListas {
         lista.add(meuFilme);
         lista.add(outroFilme);
         lista.add(lost);
-
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classificação: " + filme.getEstrelas());
+        }
     }
 }
